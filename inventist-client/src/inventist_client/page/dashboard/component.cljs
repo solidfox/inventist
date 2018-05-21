302575b76d4671ac00a7124898a7675a9e949a47
Siddhant Gupta <me@guptasiddhant.com>
2018-05-21 18:35:53 +0200

    Cleaned Dashboard

--- inventist-client/src/inventist_client/page/dashboard/component.cljs
+++ inventist-client/src/inventist_client/page/dashboard/component.cljs
@@ -1,15 +1,32 @@
 (ns inventist-client.page.dashboard.component
   (:require [remodular.core :refer [modular-component]]
+            [symbols.detailview :as s-detailview]
+            [symbols.general :as s-general]
             [inventist-client.page.dashboard.core :as core]
-            [view-dashboard.component :refer [dashboard-detail]]
+            [view-dashboard.component :refer [dashboard-detail dashboard-stats]]
+            [symbols.color :as color]
             [rum.core :refer [defc]]))
 
 (defc component < (modular-component)
   [{{state :state} :input
     trigger-event  :trigger-event}]
-  [:div {:id (str ::component-id)
-         :style {:height "100%"
-                 :display "grid"
-                 :grid-template-columns "1fr"
-                 :grid-template-rows "100%"}}
-   (dashboard-detail (core/create-dashboard-detail-args state "mock-dashboard-id"))])
+  [:div {:id    (str ::component-id)
+         :style {:height                "100%"
+                 :display               "grid"
+                 :grid-template-columns "22rem auto"
+                 :grid-template-rows    "100%"}}
+   (dashboard-stats)
+
+   [:div {:id    (str ::component-id)
+          :style {:display            "grid"
+                  :overflow           "scroll"
+                  :grid-template-rows "auto 1fr"}}
+
+    ;Toolbar
+    (s-detailview/toolbar {:items-left  (s-detailview/breadcrumb {:type "dashboard"})
+                           :items-right [(s-general/button {:color color/grey-normal
+                                                            :text  "Register Device"
+                                                            :icon  "fas fa-pen-square"})]})
+    (dashboard-detail (core/create-dashboard-detail-args state "mock-dashboard-id"))]])
+
+
--- inventist-client/src/view_dashboard/component.cljs
+++ inventist-client/src/view_dashboard/component.cljs
@@ -4,131 +4,78 @@
             [symbols.general :as s-general]
             [symbols.overview :as s-overview]
             [remodular.core :refer [modular-component]]
+            [view-person-detail.component :refer [person-detail]]
+            [inventist-client.page.people.core :as core]
             [symbols.color :as color]
             [symbols.style :as style]))
 
 (defc dashboard-detail < (modular-component)
   [{{state :state} :input
     trigger-event  :trigger-event}]
-  (let [person (get-in state [:get-dashboard-details-response :response])]
+  [:div {:id    "detail-container"
+         :style {:height             "100%"
+                 :display            "grid"
+                 :grid-template-rows "auto 1fr"}}
+   ;Error text
+   [:div {:style {:width      "100%"
+                  :height     "100%"
+                  :color      color/grey-blue
+                  :text-align "left"
+                  :top        "10rem"
+                  :margin     "2rem"
+                  :left       "2rem"}}
+    "This is the Inventist Dashboard." [:br]
+    "Students & Staff with a \"gripsholmsskolan.se\" email address will be able to see their infromation and registered devices." [:br] [:br]
+    "For non-gripsholmsskolan.se visitors, click on other sections like \"People\" and \"Inventory\" to view other's details."]
 
-    ;PEOPLE DETAILS
-    [:div {:id    "detail-container"
-           :style {:height             "100%"
-                   :display            "grid"
-                   :grid-template-rows "auto 1fr"}}
-     ;Toolbar
-     (s-detailview/toolbar {:items-left  (s-detailview/breadcrumb {:type "dashboard"
-                                                                   :item person})
-                            :items-right [(s-general/button {:color color/grey-normal
-                                                             :text  "Register Device"
-                                                             :icon  "fas fa-pen-square"})]})
-     [:div {:id    (str ::component-id)
-            :style {:display               "grid"
-                    :overflow              "scroll"
-                    :grid-template-columns "22rem 1fr"}}
-
-
-      ;Dashboard Stats
-      [:div {:style {:overflow-x      "hidden"
-                     :overflow-y      "scroll"
-                     :backgroundColor color/grey-light}}
-
-       ;Division Heading
-       (s-general/division-title
-         {:title "Inventist Stats"})
-
-       [:div {:style {:margin    "2.5rem"
-                      :display   "flex"
-                      :flex-wrap "wrap"}}
-
-        (s-general/stat-card {:value 999
-                              :text  "Total Inventory in School"})
-
-        (s-general/stat-card {:value   999
-                              :text    "Inventory per Type"
-                              :subtext [:select
-                                        [:option "All"]
-                                        [:option "All"]]})
-
-        (s-general/stat-card {:value 9999
-                              :text  "Total Computers on repair"})
-
-        (s-general/stat-card {:value   999
-                              :text    "Computer on repair per Model"
-                              :subtext [:select
-                                        [:option "All"]
-                                        [:option "All"]]})
-
-        (s-general/stat-card {:value 999
-                              :text  "Total Reserve computers"})
-
-        (s-general/stat-card {:value 0
-                              :text  "Unhandled issue reports"})]]
+   ;background image
+   [:div {:style {:width               "100%"
+                  :height              "100%"
+                  :position            "absolute"
+                  :background-image    "url(\"image/GHS-logotype-horizontal.svg\")"
+                  :background-position "35%"
+                  :background-size     "25%"
+                  :background-repeat   "no-repeat"
+                  :opacity             0.1}}]])
 
 
 
 
-      ;User Details
-      [:div {:style {:overflow-x      "hidden"
-                     :overflow-y      "scroll"
-                     :backgroundColor color/white}}
 
-       ;Division Heading
-       (s-general/division-title
-         {:title "Your Details"})
+(defc dashboard-stats []
+  ;Dashboard Stats
+  [:div {:style {:overflow-x      "hidden"
+                 :overflow-y      "scroll"
+                 :backgroundColor color/grey-light}}
 
-       ;Page Header
-       (s-detailview/detail-header
-         {:image         (:image person)
-          :heading       (str (:fname person) " " (:lname person))
-          :sub-heading-1 (:type person)
-          :sub-heading-2 (:group person)})
+   ;Division Heading
+   (s-general/division-title
+     {:title "Inventist Stats"})
 
-       ;Information
-       (s-detailview/section-information
-         {:fields      [{:label    "Email"
-                         :value    (str (:email person))
-                         :editable false}
-                        {:label    "Phone"
-                         :value    (:phone person)
-                         :editable false}
-                        {:label    "Gender"
-                         :value    (cond (= (:sex person) "f") "Female"
-                                         (= (:sex person) "m") "Male")
-                         :editable false}
-                        {:label    "Address"
-                         :value    (:address person)
-                         :editable false}
-                        {:label    "Assigned Devices"
-                         :value    (count (:inventory person))
-                         :editable false}]
-          :edit-mode   false
-          :enable-edit false})
+   [:div {:style {:margin    "2.5rem"
+                  :display   "flex"
+                  :flex-wrap "wrap"}}
 
-       ;Assigned Devices
-       [:div {:style {:margin         "1rem 2.5rem 1rem"
-                      :display        "flex"
-                      :flex-direction "row"}
-              :id    "devices"}
-        (s-detailview/section-left)
-        [:div {:style {:margin "0 0 0 1rem" :display "flex" :flex-direction "column"
-                       :width  "100%"}}
-         (s-detailview/section-title {:title "Assigned Devices"
-                                      :buttons [(s-detailview/section-title-button {:icon     "fas fa-plus-circle"
-                                                                                    :text     "Assign new devices"
-                                                                                    :on-click ""})]})
+    (s-general/stat-card {:value 999
+                          :text  "Total Inventory in School"})
 
-         [:div {:style {:display        "flex"
-                        :flex-direction "row"
-                        :flex-wrap      "wrap"
-                        :align-items    "flex-start"}}
-          (for [item (:inventory person)]
-            (s-detailview/device-card {:item item}))]
-         (s-detailview/section-divider)]]
+    (s-general/stat-card {:value   999
+                          :text    "Inventory per Type"
+                          :subtext [:select
+                                    [:option "All"]
+                                    [:option "All"]]})
 
-       ;Timeline
-       (s-detailview/section-timeline {:type           "people"
-                                       :enable-comment false
-                                       :history        (:history person)})]]]))
+    (s-general/stat-card {:value 9999
+                          :text  "Total Computers on repair"})
 
+    (s-general/stat-card {:value   999
+                          :text    "Computer on repair per Model"
+                          :subtext [:select
+                                    [:option "All"]
+                                    [:option "All"]]})
+
+    (s-general/stat-card {:value 999
+                          :text  "Total Reserve computers"})
+
+    (s-general/stat-card {:value 0
+                          :text  "Unhandled issue reports"})]])
