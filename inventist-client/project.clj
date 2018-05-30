3ca70cdeb8461f11dbb82c6fe1cfad84a21b5bc3
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-05-30 09:59:00 +0200

    Switched to using optimized build for publishing

--- inventist-client/project.clj
+++ inventist-client/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
 
 
@@ -30,96 +30,89 @@
   :source-paths ["src"]
 
   :cljsbuild {:builds
-              [{:id "dev"
-                :source-paths ["src"]
+              {:dev
+               {:source-paths ["src"]
 
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
-                :figwheel {:on-jsload "inventist-client.main/on-js-reload"}
-                           ;; :open-urls will pop open your application
-                           ;; in the default browser once Figwheel has
-                           ;; started and compiled your application.
-                           ;; Comment this out once it no longer serves you.
-                           ;; :open-urls ["http://localhost:3449/index.html"]}
+                :figwheel     {:on-jsload "inventist-client.main/on-js-reload"}
 
-                :compiler {:main inventist-client.main
-                           :asset-path "/js/compiled/out"
-                           :output-to "resources/public/js/compiled/inventist_client.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
-                           :preloads [devtools.preload]}}
-                           ;:install-deps true
-                           ;:npm-deps {:react-firebaseui "1.2.3"
-                           ;           :firebase "4.12.1"}}}
+                :compiler     {:main                 inventist-client.main
+                               :asset-path           "/js/compiled/out"
+                               :output-to            "resources/public/js/compiled/inventist_client.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true
+                               :preloads             [devtools.preload]}}
+               ;:install-deps true
+               ;:npm-deps {:react-firebaseui "1.2.3"
+               ;           :firebase "4.12.1"}}}
                ;; This next build is a compressed minified build for
                ;; production. You can build this with:
                ;; lein cljsbuild once min
-               {:id "min"
-                :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/inventist_client.js"
-                           :main inventist-client.main
-                           :optimizations :simple
-                           :pretty-print false}}]}
+               :min
+               {:source-paths ["src"]
+                :compiler     {:output-to     "resources/public/js/compiled/inventist_client.js"
+                               :main          inventist-client.main
+                               :optimizations :advanced
+                               :pretty-print  false}}}}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs       ["resources/public/css"]       ;; watch and update CSS
 
-             :ring-handler server.router/handle-request
+             :ring-handler   server.router/handle-request
              :server-logfile "/tmp/figwheel-log.txt"}
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
+  ;; Start an nREPL server into the running figwheel process
+  ;; :nrepl-port 7888
 
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
+  ;; Server Ring Handler (optional)
+  ;; if you want to embed a ring handler into the figwheel http-kit
+  ;; server, this is for simple ring servers, if this
 
-             ;; doesn't work for you just run your own server :) (see lein-ring)
+  ;; doesn't work for you just run your own server :) (see lein-ring)
 
-             ;; :ring-handler hello_world.server/handler
+  ;; :ring-handler hello_world.server/handler
 
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
+  ;; To be able to open files in your editor from the heads up display
+  ;; you will need to put a script on your path.
+  ;; that script will have to take a file path and a line number
+  ;; ie. in  ~/bin/myfile-opener
+  ;; #! /bin/sh
+  ;; emacsclient -n +$2 $1
+  ;;
+  ;; :open-file-command "myfile-opener"
 
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
+  ;; if you are using emacsclient you can just use
+  ;; :open-file-command "emacsclient"
 
-             ;; if you want to disable the REPL
-             ;; :repl false
+  ;; if you want to disable the REPL
+  ;; :repl false
 
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+  ;; to configure a different figwheel logfile path
+  ;; :server-logfile "tmp/logs/figwheel-logfile.log"
 
-             ;; to pipe all the output to the repl
-             ;; :server-logfile false
+  ;; to pipe all the output to the repl
+  ;; :server-logfile false
 
 
 
   ;; Setting up nREPL for Figwheel and ClojureScript dev
   ;; Please see:
   ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]
-                                  [figwheel-sidecar "0.5.15"]
-                                  [com.cemerick/piggieback "0.2.2"]]
-                   ;; need to add dev source path here to get user.clj loaded
-                   :source-paths ["src" "dev"]
-                   ;; for CIDER
-                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
-                   :repl-options {:port 8230
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-                   ;; need to add the compliled assets to the :clean-targets
-                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
-                                                     :target-path]}
+  :profiles {:dev            {:dependencies  [[binaryage/devtools "0.9.9"]
+                                              [figwheel-sidecar "0.5.15"]
+                                              [com.cemerick/piggieback "0.2.2"]]
+                              ;; need to add dev source path here to get user.clj loaded
+                              :source-paths  ["src" "dev"]
+                              ;; for CIDER
+                              ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                              :repl-options  {:port             8230
+                                              :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                              ;; need to add the compliled assets to the :clean-targets
+                              :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                                :target-path]}
              :figwheel-nrepl {:repl-options {:init (do (require 'user)
                                                        (user/fig-start))}}})
